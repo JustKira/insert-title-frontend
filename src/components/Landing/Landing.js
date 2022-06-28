a76eff819a09e9ede63c9db2c10b36cb949eb16d
JustKira <SHighGamerHG@Gmail.com>
2022-06-28 15:24:36 +0200

    feat: LandingPage & Navbar

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Button from '../Buttons/Button'
+
+//import images
+import {ReactComponent as Hero} from '../../svg/hero.svg'
+
+//Frist Part of Landing Page
+const Landing = () => {
+  return (
+    <div className='flex items-center justify-between w-full h-screen p-10'>
+        <div>
+            <h1 className='text-blue-400 font-bold text-6xl font'>INSERT TITLE</h1>
+            <h2 className='font-light text-3xl'>WE HELP YOU REACH YOUR GOAL</h2>
+            <h3 className='font-extralight text-xl'>WE PROVIDE YOU WITH , INTERNSHIPS , CV-BUILDER AND ROADMAP TO UP YOUR SKILLS</h3>
+            <Button>Hello world</Button>
+            <Button hollow={true}>Hello world</Button>
+        </div>
+        <div>
+          <Hero width='600' height='600' className='drop-shadow-blueglow' />
+        </div>
+    </div>
+  )
+}
+
+export default Landing
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            dropShadow: {
+                blueglow: '0px 10px 8px #1088CC'
+            },
+            fontFamily: {
+                Pop: ['Poppins', 'sans-serif']
+            }
+        }
+    },
+    plugins: []
+}
