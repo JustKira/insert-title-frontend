90ac4e8fd5ddcf742df683fd1d70b642fb1417f5
mohamed tarek <mt1348@fayoum.edu.eg>
2022-06-29 04:59:15 +0200

    posts

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Posts from './pages/Posts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
   <Routes>
-    <Route path='/' element={<App/>}/>
+    <Route path='/posts' element={<Posts/>}/>
   </Routes>
 </Router>
 
